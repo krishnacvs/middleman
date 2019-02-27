09483dfe8b5313ac066799b1245bcac366dd553f
krishnacvs@yahoo.com <krishnacvs@yahoo.com>
2019-02-27 13:03:09 -0500

    Module Login , Register , Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgentsComponent } from './agents/agents.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AgentsComponent
+    AgentsComponent,
+    MenuComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
